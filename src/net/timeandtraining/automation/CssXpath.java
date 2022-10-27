fd84f297b1e96452879f9211b6ccec7752280c63
NusrumRashid <nusrumr@gmail.com>
2022-10-27 15:38:28 -0700

    CssXpath,Readme

---
+++ src/net/timeandtraining/automation/CssXpath.java
@@ -0,0 +1,18 @@
+package net.timeandtraining.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CssXpath {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = Base.getDriver();
+		
+		driver.get("http://automationpractice.com");
+		driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[3]/a")).click();
+		
+		
+	}
+
+}
--- src/net/timeandtraining/automation/LoginTest.java
+++ src/net/timeandtraining/automation/LoginTest.java
@@ -21,6 +21,27 @@ public class LoginTest {
 	 driver.findElement(By.id("passwd")).sendKeys("123456789");
 	 
 	 driver.findElement(By.id("SubmitLogin")).click();
+	 
+	// WebElement signout_button = driver.findElement(By.className("logout"));
+	 WebElement page_header = driver.findElement(By.className("page-heading"));
+	 
+	// String signout_button_text = signout_button.getText();
+	 String page_header_text = page_header.getText();
+	 
+	 
+	 //System.out.println(signout_button_text);
+	 //System.out.println(page_header_text);
+	 
+	 if(page_header_text.equals("MY ACCOUNT"
+)) {System.out.println("Login test is Passed");
+}else {
+	System.out.println("Login test is Failed");
+	
+	
+};
+
+	
 	}
+	
 
 }
