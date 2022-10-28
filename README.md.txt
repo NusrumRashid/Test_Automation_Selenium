8bc5cc874544cea039cc635d5a38a3a8ce71ded3
NusrumRashid <nusrumr@gmail.com>
2022-10-28 02:45:02 -0700

    CSS

--- README.md.txt
+++
@@ -1,9 +0,0 @@
-Ecommerce Website-Automation using Selenium
-
-Technologies:  Selenium Webdriver, Java
-
-Here I automate the following features:
-  o Signup
-  o Login
-  o Reading any header text,element of the site
-  o Search using JUnit
--- src/net/timeandtraining/automation/CssXpath.java
+++ src/net/timeandtraining/automation/CssXpath.java
@@ -2,6 +2,7 @@ package net.timeandtraining.automation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CssXpath {
 
@@ -10,7 +11,21 @@ public class CssXpath {
 		WebDriver driver = Base.getDriver();
 		
 		driver.get("http://automationpractice.com");
-		driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[3]/a")).click();
+        driver.findElement(By.className("login")).click();
+		
+		
+		
+	 driver.findElement(By.id("email")).sendKeys("rashidnusrum@gmail.com");
+		
+	 driver.findElement(By.id("passwd")).sendKeys("123456789");
+	 
+	 driver.findElement(By.id("SubmitLogin")).click();
+		
+		//driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[3]/a")).click();
+	 
+	 driver.findElement(By.cssSelector("a[href^=\"http://automationpractice.com/index.php?controller=contact\"]")).click();
+		
+		
 		
 		
 	}
